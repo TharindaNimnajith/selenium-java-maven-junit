ad2d3742bd71928da5a8b3afd40ae08576b0bb60
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2023-01-07 23:41:46 +0530

    init

--- src/test/java/com/example/seleniumjavamavenjunit/MainPageTest.java
+++ src/test/java/com/example/seleniumjavamavenjunit/MainPageTest.java
@@ -4,13 +4,15 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.*;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainPageTest {
     MainPage mainPage = new MainPage();
